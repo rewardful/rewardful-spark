5a6746d15edc21863b1bfcffbaafcd5a29daa520
Mina Abadir <mina@abadir.email>
2019-09-22 14:40:49 -0400

    Rewardful Spark 3.0
    Rewardul Spark for Laravel Spark 9.0+

---
+++ README.md
@@ -0,0 +1,25 @@
+# Introduction
+
+RewardfulSpark is a small library that allows you to integrate your Laravel Spark application with Rewardful with minimal configuration.
+
+# Installation
+
+First, require the RewardfulSpark package with Composer
+
+`composer require rewardful/rewardful-spark`
+
+# Usage
+
+For the complete documentation on how integrate this library into your Laravel Spark application, please check the official documentation.
+
+# Changelog
+
+Please see CHANGELOG for more information what has changed recently.
+
+# Credits
+
+Mina Abadir
+
+# License
+
+The MIT License (MIT). Please see License File for more information.
---
+++ src/UpdateStripePaymentMethod.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Rewardful\RewardfulSpark;
+
+use Laravel\Spark\User;
+use Laravel\Spark\Spark;
+use Laravel\Spark\Contracts\Repositories\UserRepository;
+use Laravel\Spark\Contracts\Repositories\TeamRepository;
+use Laravel\Spark\Contracts\Interactions\Settings\PaymentMethod\UpdatePaymentMethod as Contract;
+use Laravel\Spark\Interactions\Settings\PaymentMethod\UpdateStripePaymentMethod as UpdatePaymentMethod;
+
+class UpdateStripePaymentMethod extends UpdatePaymentMethod implements Contract
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handle($billable, array $data)
+    {
+        // Next, we need to check if this application is storing billing addresses and if so
+        // we will update the billing address in the database so that any tax information
+        // on the user will be up to date via the taxPercentage method on the billable.
+        if (Spark::collectsBillingAddress()) {
+            Spark::call(
+                $this->updateBillingAddressMethod($billable),
+                [$billable, $data]
+            );
+        }
+
+        if (! $billable->stripe_id) {
+            $billable->createAsStripeCustomer([
+                'metadata' =>[
+                    'referal' => $data['referral']
+                ]
+            ]);
+        }
+
+        $billable->updateDefaultPaymentMethod($data['stripe_payment_method']);
+    }
+}
