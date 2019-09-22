fdf536fb8ebf8514538eda801000ea39adc5458d
Mina Abadir <mina@abadir.email>
2019-09-22 19:10:18 -0400

    RewardfulSpark 1.0
    Rewardful Spark for Laravel Spark 5.5 - 5.8

--- src/resources/js/rewardful-register.js
+++ src/resources/js/rewardful-register.js
@@ -2,9 +2,13 @@ module.exports = {
     mounted(){
         window.addEventListener('Rewardful.tracked', () => {
             if(this.$options.name === 'spark-register-stripe')
+            {
                 this.registerForm.referral = Rewardful.referral
+            }
             if(this.$options.name === 'spark-subscribe-stripe')
+            {
                 this.form.referral = Rewardful.referral
+            }
           });
     }
 }
